26bb780d5951444d9462c22ae8c3f904d56ef3c4
tobiakinlade <tobi.akinlade@yahoo.com>
2022-07-19 17:18:06 +0100

    generate invite link

---
+++ pages/room/[id]/join.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Join() {
+  return (
+    <>
+      <h1 className='mt-20 mb-20 text-center text-3xl uppercase font-black'>
+        Room invite
+      </h1>
+    </>
+  )
+}
